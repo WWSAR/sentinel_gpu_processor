33d80f68652b66709c6ba6dbaab76dcfc1ecfc0c
aik9508 <ke.douglas.wang@hotmail.com>
2026-03-08 15:57:50 +0800

    fix: no longer initialize cuda array by reading zero chunks from saved images

--- s1proc/csrc/src/geo2rdr_reramp.cu
+++ s1proc/csrc/src/geo2rdr_reramp.cu
@@ -28,7 +28,7 @@ bool file_exists(const std::string& filename) {
 
 __global__
 void reproject(short int *dem, Complex *burstdata, double *deramp_phase,
-               Complex *outdata, int *burst_num,
+               Complex *outdata, Complex *overlapdata,
                double *tt, double *xx, double *vv, const std::size_t nstatvec,
                const double tmid, double *xmid, double *vmid,
                const double latmax, const double lonmin, const double dlat, 
@@ -40,8 +40,7 @@ void reproject(short int *dem, Complex *burstdata, double *deramp_phase,
                const int lines_per_burst, const int first_valid_line,
                const int last_valid_line, const int first_valid_sample,
                const int last_valid_sample, const double wvl,
-               const int current_burst_num, const bool written,
-               const std::size_t n){
+               const bool written, const std::size_t n){
     std::size_t index = blockIdx.x * blockDim.x + threadIdx.x;
     std::size_t stride = blockDim.x * gridDim.x;
     int row, col, intr, inta;
@@ -50,7 +49,10 @@ void reproject(short int *dem, Complex *burstdata, double *deramp_phase,
     double tline, rngpix, rgoff, azoff, fracr, fraca, llh[3], xyz[3], dr[3];
     double reramp1, reramp2, reramp, phase1, phase2, phase3, phase4, phase;
     double resx, resy, cosphase, sinphase;
-    Complex cpx1, cpx2, burst1, burst2, burst3, burst4, res;
+    Complex cpx1, cpx2, burst1, burst2, burst3, burst4, res, zero;
+    bool overlapped;
+    zero.x = 0;
+    zero.y = 0;
     for (std::size_t i = index; i < n; i += stride){
         row = i/nlon; 
         col = i - nlon*row;
@@ -60,19 +62,24 @@ void reproject(short int *dem, Complex *burstdata, double *deramp_phase,
         if (written){
             res = outdata[i];
         }else{
-            // initialize res to zero
             res.x = 0;
             res.y = 0;
+            outdata[i] = zero;
         }
         if (lat > burst_latmax || lat < burst_latmin ||
             lon > burst_lonmax || lon < burst_lonmin){
-            outdata[i] = res;
+            if (!written){
+                outdata[i] = zero;
+                overlapdata[i] = zero; 
+            }
             continue;
         }
-        if (res.x != 0 && res.y !=0){
+        if (res.x != 0 || res.y !=0){
             // if res is not zero, it means this pixel has been processed
-            // in a previous burst, so we skip it
-            continue;
+            // in a previous burst, so we need to save it to overlapdata
+            overlapped = true;
+        }else{
+            overlapped = false;
         }
         llh[0] = lat;
         llh[1] = lon;
@@ -90,8 +97,8 @@ void reproject(short int *dem, Complex *burstdata, double *deramp_phase,
         //    printf("rgoff: %f, azoff: %f\n",rgoff,azoff);
         //    printf("\n\n\n");
         //}
-        if (rgoff > first_valid_sample && rgoff < last_valid_sample &&
-            azoff >= first_valid_line+5 && azoff <= last_valid_line-5){
+        if (rgoff >= first_valid_sample && rgoff < last_valid_sample &&
+            azoff >= first_valid_line+5 && azoff < last_valid_line-5){
             intr = int(rgoff);
             fracr = rgoff - intr;
             inta = int(azoff);
@@ -122,14 +129,21 @@ void reproject(short int *dem, Complex *burstdata, double *deramp_phase,
             sinphase = sin(phase);
             res.x = resx*cosphase - resy*sinphase;
             res.y = resx*sinphase + resy*cosphase;
+        }else{
+            continue;
         }
-        outdata[i] = res;
-        burst_num[i] = current_burst_num;
+        if (overlapped){
+            overlapdata[i] = res;
+        }
+        else{
+            outdata[i] = res;
+        }
     }
 }
 
 int geo2rdr_reramp(const std::string &dbname,
                    const std::string &slcoutfile,
+                   const std::string &overlapfile,
                    std::string &slcinfile){
     sqlite3 *db; // database that stores relevant paramters
 
@@ -138,37 +152,30 @@ int geo2rdr_reramp(const std::string &dbname,
         std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
         return 1;
     }
+    // database table name
     const std::string tblname = "file";
-    const std::string burst_num_file = slcoutfile+ ".burst_num";
     std::string orbfile, demfile, demrscfile;
     std::size_t nstatvec, burstsize;
     rsc demrsc;
     int azimuth_bursts, lines_per_burst, nrange, blockSize=256, numBlocks;
-    int batch_lines = 3000, nbatch, subswath;
+    int batch_lines = 3000, nbatch;
     short int *dem, *d_dem;
     double prf, wvl,slant_range_time,range_sampling_rate;
     double *startime, *tt, *xx, *vv, *xmid, *vmid;
     double *d_tt, *d_xx, *d_vv, *d_xmid, *d_vmid;
     int *first_valid_line, *last_valid_line;
     int *first_valid_sample, *last_valid_sample;
-    int *burst_num; // index of burst for each pixel
-    int *d_burst_num; // device copy of burst_num
     Complex *burstdata, *d_burstdata, *outdata, *d_outdata;
+    Complex *overlapdata, *d_overlapdata;
     double *deramp_phase, *d_deramp_phase; 
     bool written;
-    char last_char;
 
-    last_char = dbname.back();
-    subswath = last_char - '0';
     nrange = get_parami(db,tblname,"samplesPerBurst");
     lines_per_burst = get_parami(db,tblname,"linesPerBurst");
     azimuth_bursts = get_parami(db,tblname,"azimuthBursts");
     prf = get_paramd(db,tblname,"prf");
     wvl = get_paramd(db,tblname,"wvl");
-    //azimuth_time_interval = get_paramd(db,tblname,"azimuthTimeInterval");
     range_sampling_rate = get_paramd(db,tblname,"rangeSamplingRate");
-    //rawdataprf = get_paramd(db,tblname,"rawdataprf");
-    //radar_frequency = get_paramd(db,tblname,"radarFrequency");
     slant_range_time = get_paramd(db,tblname,"slantRangeTime");
     orbfile = get_params(db,tblname,"orbinfo");
     burstsize = nrange*lines_per_burst;
@@ -221,16 +228,16 @@ int geo2rdr_reramp(const std::string &dbname,
     read_orbit_ascii(orbfile,nstatvec,&tt,&xx,&vv);
     //std::cout << "orbfile: " << orbfile << std::endl;
     outdata = (Complex*)malloc(sizeof(Complex)*demrsc.nlon*batch_lines);
+    overlapdata = (Complex*)malloc(sizeof(Complex)*demrsc.nlon*batch_lines);
     dem = (short int*)malloc(sizeof(short int)*demrsc.nlon*batch_lines);
-    burst_num = (int*)malloc(sizeof(int)*demrsc.nlon*batch_lines);
     cudaMalloc((void**)&d_tt,sizeof(double)*nstatvec);
     cudaMalloc((void**)&d_xx,sizeof(double)*nstatvec*3);
     cudaMalloc((void**)&d_vv,sizeof(double)*nstatvec*3);
     cudaMalloc((void**)&d_xmid,sizeof(double)*3);
     cudaMalloc((void**)&d_vmid,sizeof(double)*3);
     cudaMalloc((void**)&d_dem,sizeof(short int)*demrsc.nlon*batch_lines);
-    cudaMalloc((void**)&d_burst_num,sizeof(int)*demrsc.nlon*batch_lines);
     cudaMalloc((void**)&d_outdata,sizeof(Complex)*demrsc.nlon*batch_lines);
+    cudaMalloc((void**)&d_overlapdata,sizeof(Complex)*demrsc.nlon*batch_lines);
     cudaMalloc((void**)&d_burstdata,sizeof(Complex)*nrange*lines_per_burst);
     cudaMalloc((void**)&d_deramp_phase,sizeof(double)*nrange*lines_per_burst);
 
@@ -239,6 +246,7 @@ int geo2rdr_reramp(const std::string &dbname,
     cudaMemcpy(d_vv,vv,sizeof(double)*nstatvec*3,cudaMemcpyHostToDevice);
     nbatch = (demrsc.nlat + batch_lines-1)/batch_lines;
     for (int ibatch = 0; ibatch < nbatch; ++ibatch){
+        // calculate the first and the last lines of current batch of output file
         std::size_t line_start = ibatch * batch_lines;
         std::size_t line_end = line_start + batch_lines;
         line_end = line_end < demrsc.nlat ? line_end : demrsc.nlat;
@@ -246,13 +254,6 @@ int geo2rdr_reramp(const std::string &dbname,
         readdem(demfile,line_start*demrsc.nlon,nlines*demrsc.nlon,dem);
         cudaMemcpy(d_dem,dem,sizeof(short int)*nlines*demrsc.nlon,
                 cudaMemcpyHostToDevice);
-        // read .geo file if current subswath is > 1
-        if (file_exists(slcoutfile)){
-            std::cout << slcoutfile << " exists." << std::endl;
-            read_cpx(slcoutfile,line_start*demrsc.nlon,nlines*demrsc.nlon,outdata);
-            cudaMemcpy(d_outdata,outdata,sizeof(Complex)*nlines*demrsc.nlon,
-                        cudaMemcpyHostToDevice);
-        }
         numBlocks = (nlines*demrsc.nlon+blockSize-1)/blockSize;
         std::cout << "Batch " << ibatch << ", nlines: " << nlines << std::endl;
         
@@ -260,7 +261,7 @@ int geo2rdr_reramp(const std::string &dbname,
             double tstart, dtaz, tend, tmid;
             double rngstart, dmrg, rngend;
             double latlons[4];
-            if (!file_exists(slcoutfile) & iburst == 0){
+            if (iburst == 0){
                 written = false;
             }else{
                 written = true;
@@ -270,52 +271,47 @@ int geo2rdr_reramp(const std::string &dbname,
             tend = tstart + (lines_per_burst-1)*dtaz;
             tmid = (tstart + tend)*0.5;
             intp_orbit(nstatvec, tt,xx,vv,tmid,xmid,vmid);
-            //std::cout << "Burst " << (iburst+1) << " / " << azimuth_bursts << std::endl;
-            //std::cout << "Start Acquisition time: " << tstart << std::endl;
-            //std::cout << "Stop Acquisition time: " << tend << std::endl;
             rngstart = slant_range_time*SOL/2.0;
             dmrg = SOL/2.0/range_sampling_rate;
             rngend = rngstart + (nrange-1)*dmrg;
-            //std::cout << "rngstart: " << rngstart << std::endl;
-            //std::cout << "rngend: " << rngend << std::endl;
-            //std::cout << slcinfile << std::endl;
-            read_cpx(slcinfile,iburst*burstsize,burstsize,burstdata);
-            read_double("deramp_phase",iburst*burstsize,burstsize,deramp_phase);
+
+            read_binary<Complex>(slcinfile,iburst*burstsize,burstsize,
+                    burstdata);
+            read_binary<double>("deramp_phase",iburst*burstsize,burstsize,
+                    deramp_phase);
+
+            // calculate lat/lon bnoudaries of current burst
             bounds(tstart,tend,rngstart,rngend,tt,xx,vv,nstatvec,latlons,"RIGHT");
-            //std::cout << "latlons: " << latlons[0] << " "<< latlons[1] << " "<< latlons[2]<< " "<< latlons[3] << std::endl;
             cudaMemcpy(d_burstdata,burstdata,sizeof(Complex)*burstsize,
                     cudaMemcpyHostToDevice);
             cudaMemcpy(d_deramp_phase,deramp_phase,sizeof(double)*burstsize,
                     cudaMemcpyHostToDevice);
             cudaMemcpy(d_xmid,xmid,sizeof(double)*3,cudaMemcpyHostToDevice);
             cudaMemcpy(d_vmid,vmid,sizeof(double)*3,cudaMemcpyHostToDevice);
+            
+            // reprojection
             reproject<<<numBlocks,blockSize>>>(d_dem,d_burstdata,d_deramp_phase,
-            d_outdata,d_burst_num,d_tt,d_xx,d_vv,nstatvec,tmid,d_xmid,d_vmid, 
+            d_outdata,d_overlapdata,d_tt,d_xx,d_vv,nstatvec,tmid,d_xmid,d_vmid, 
             demrsc.latmax+demrsc.dlat*line_start,demrsc.lonmin,
             demrsc.dlat,demrsc.dlon,demrsc.nlon,
             latlons[0],latlons[1],latlons[2],latlons[3],rngstart,tstart,dmrg,dtaz,
             nrange, lines_per_burst, first_valid_line[iburst],
             last_valid_line[iburst], first_valid_sample[iburst],
-            last_valid_sample[iburst], wvl, iburst+1, written,nlines*demrsc.nlon);
+            last_valid_sample[iburst], wvl, written, nlines*demrsc.nlon);
             cudaDeviceSynchronize();
         }
         cudaMemcpy(outdata,d_outdata,sizeof(Complex)*demrsc.nlon*nlines,
                 cudaMemcpyDeviceToHost);
-        cudaMemcpy(burst_num,d_burst_num,sizeof(int)*demrsc.nlon*nlines,
+        cudaMemcpy(overlapdata,d_overlapdata,sizeof(Complex)*demrsc.nlon*nlines,
                 cudaMemcpyDeviceToHost);
-        if (!file_exists(slcoutfile)){
-            if (ibatch == 0){
-                save_cpx(outdata,false,demrsc.nlon*nlines,slcoutfile);
-                //save_int(burst_num,false,demrsc.nlon*nlines,burst_num_file);
-            }else{
-                save_cpx(outdata,true,demrsc.nlon*nlines,slcoutfile);
-                //save_int(burst_num,true,demrsc.nlon*nlines,burst_num_file);
-            }
+        if (ibatch == 0){
+            save_binary<Complex>(outdata,false,demrsc.nlon*nlines,slcoutfile);
+            save_binary<Complex>(overlapdata,false,demrsc.nlon*nlines,
+                    overlapfile);
         }else{
-            save_cpx(outdata,std::size_t(line_start*demrsc.nlon),
-                     std::size_t(nlines*demrsc.nlon),slcoutfile);
-            //save_int(burst_num,std::size_t(line_start*demrsc.nlon),
-            //        std::size_t(nlines*demrsc.nlon),burst_num_file);
+            save_binary<Complex>(outdata,true,demrsc.nlon*nlines,slcoutfile);
+            save_binary<Complex>(overlapdata,true,demrsc.nlon*nlines,
+                    overlapfile);
         }
     }
 
@@ -327,7 +323,6 @@ int geo2rdr_reramp(const std::string &dbname,
     free(burstdata);
     free(deramp_phase);
     free(dem);
-    free(burst_num);
     free(tt);
     free(xx);
     free(vv);
@@ -336,7 +331,6 @@ int geo2rdr_reramp(const std::string &dbname,
     cudaFree(d_burstdata);
     cudaFree(d_deramp_phase);
     cudaFree(d_dem);
-    cudaFree(d_burst_num);
     cudaFree(d_tt);
     cudaFree(d_xx);
     cudaFree(d_vv);
@@ -346,15 +340,18 @@ int geo2rdr_reramp(const std::string &dbname,
 }
 
 int main(int argc, char *argv[]){
-    if (argc<3){
-        std::cout << "Usage: geo2rdr_reramp dbname slcoutfile [slcinfile]" << std::endl;
+    if (argc<4){
+        std::cout << "Usage: geo2rdr_reramp dbname slcoutfile overlapfile " <<
+            "[slcinfile]" << std::endl;
+        return 0;
     }
     const std::string dbname = std::string(argv[1]);
     const std::string slcoutfile = std::string(argv[2]);
+    const std::string overlapfile = std::string(argv[3]);
     std::string slcinfile = "";
-    if (argc>3){
-        slcinfile = std::string(argv[3]);
+    if (argc>4){
+        slcinfile = std::string(argv[4]);
     }
-    geo2rdr_reramp(dbname,slcoutfile,slcinfile);
+    geo2rdr_reramp(dbname,slcoutfile,overlapfile,slcinfile);
     return 0;
 }
