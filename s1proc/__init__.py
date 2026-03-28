8ec3b01931ab44dc951fe98ceb58ac83d9ec9bec
Ke Wang <ke.douglas.wang@hotmail.com>
2026-03-28 17:54:21 +0800

    fix: fix a syntax error

--- s1proc/__init__.py
+++ s1proc/__init__.py
@@ -4,10 +4,8 @@ from pathlib import Path
 
 __version__ = "0.1.0"
 
-# 获取当前 s1proc 包的根目录（绝对路径）
 BASE_DIR = Path(__file__).resolve().parent
 
-# 定义二进制文件夹路径
 BIN_DIR = os.path.join(BASE_DIR, "bin")
 
 def get_bin_path(bin_name: str) -> str:
@@ -19,8 +17,7 @@ def get_bin_path(bin_name: str) -> str:
 
     bin_path = os.path.join(BIN_DIR, bin_name)
 
-    if not bin_path.exists():
-        # 这里可以抛出一个更友好的异常，提醒用户可能没编译成功
+    if not os.path.exists(bin_path):
         raise FileNotFoundError(
             f"Cannot find executable: {bin_path}\n"
             f"Make sure the package is correctly installed via 'pip install -e .'"
