52956607caabc841624758cdbf63cac87a4448c4
Ke Wang <ke.douglas.wang@hotmail.com>
2026-06-19 22:50:34 +0800

    add PREFIX/Library to CMAKE_PREFIX_PATH

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Using NVCC: $(nvcc --version | head -n 1)"
 cmake -S csrc -B build -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="$PREFIX/s1proc" \
-    -DCMAKE_PREFIX_PATH="$PREFIX"
+    -DCMAKE_PREFIX_PATH="$PREFIX;$PREFIX/Library"
     
 cmake --build build --target install -- -j$(nproc)
 
